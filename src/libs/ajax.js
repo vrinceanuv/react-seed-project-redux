c6d1e4e1ae72c37bbcb3b032fe6a75fe4b929506
vrinceanuv <vrinceanu.valentin@gmail.com>
2017-06-19 19:59:46 +0100

    refactoring small bits

--- src/libs/ajax.js
+++ src/libs/ajax.js
@@ -1,25 +1,23 @@
 import api from 'superagent-wrapper';
 import * as actions from '../actions';
-import * as url from '../constants/apiConstants'
+import * as url from '../constants/apiConstants';
 
-export const loadItems = () => {
-  return function (dispatch) {
-    return api.get(url.ITEMS_URL)
-      .then(items => {
-        dispatch(actions.updateItems(items))
-        dispatch(actions.updateLoader(true))
-      })
-  }
+export const loadItems = () => dispatch => {
+  return api.get(url.ITEMS_URL)
+    .then(items => {
+      dispatch(actions.updateItems(items))
+      dispatch(actions.updateLoader(true))
+    })
 }
 
-export const createItem = (item) => {
+export const createItem = item => {
   return api.post(url.ITEMS_URL, item)
 }
 
-export const updateItem = (item) => {
+export const updateItem = item => {
   return api.put(`${url.ITEMS_URL}/${item.id}`, item)
 }
 
-export const deleteItem = (id) => {
+export const deleteItem = id => {
   return api.del(`${url.ITEMS_URL}/${id}`)
 }
